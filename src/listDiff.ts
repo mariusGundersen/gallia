309a64c0f3714c37f78fb903da15660cdeb42997
Marius Gundersen <marius.gundersen@clave.no>
2021-01-28 11:54:43 +0100

    made it generic

--- src/listDiff.ts
+++ src/listDiff.ts
@@ -1,28 +1,30 @@
 type Change = "INSERT" | "UPDATE" | "DELETE";
 
-export default function listDiff(
-  listA: string[],
-  listB: string[],
-  callback: (change: Change, index: number, key: string) => {}
+type Keyed = { key: string };
+
+export default function listDiff<T extends Keyed>(
+  listA: T[],
+  listB: T[],
+  callback: (change: Change, index: number, item: T) => {}
 ) {
   let a = 0;
   let b = 0;
   const la = listA.length;
   const lb = listB.length;
 
-  const mapA = new Map(listA.map((key, index) => [key, index]));
-  const mapB = new Map(listB.map((key, index) => [key, index]));
+  const mapA = new Map(listA.map(({ key }, index) => [key, index]));
+  const mapB = new Map(listB.map(({ key }, index) => [key, index]));
 
   for (; a < la && b < lb; ) {
     const itemA = listA[a];
     const itemB = listB[b];
 
-    if (itemA === itemB) {
+    if (itemA.key === itemB.key) {
       a++;
       b++;
     } else {
-      const indexOfItemAInListB = mapB.get(itemA);
-      const indexOfItemBInListA = mapA.get(itemB);
+      const indexOfItemAInListB = mapB.get(itemA.key);
+      const indexOfItemBInListA = mapA.get(itemB.key);
 
       if (
         indexOfItemAInListB === undefined &&
